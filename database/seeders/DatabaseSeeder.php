42946a2505e8103322d8f21d2da8c07539a47aae
Abdullah_Kurdi <kurdi313@gmail.com>
2022-03-06 00:10:30 +0200

    chat system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,34 +17,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create(['name' => 'Sami', 'email' => 'sami@gmail.com', 'email_verified_at' => now(), 'password' => bcrypt('123123123'), 'remember_token' => Str::random(10),]);
+        User::create(['name'=>'Abdullah', 'email'=>'abdullah@gmail.com', 'email_verified_at'=>now(), 'password'=>bcrypt('123456789'), 'remember_token'=>Str::random(10),]);
+        User::create(['name'=>'Mohammed', 'email'=>'mohammed@gmail.com', 'email_verified_at'=>now(), 'password'=>bcrypt('123456789'), 'remember_token'=>Str::random(10),]);
+        User::create(['name'=>'Murad', 'email'=>'murad@gmail.com', 'email_verified_at'=>now(), 'password'=>bcrypt('123456789'), 'remember_token'=>Str::random(10),]);
+        User::create(['name'=>'Ali', 'email'=>'ali@gmail.com', 'email_verified_at'=>now(), 'password'=>bcrypt('123456789'), 'remember_token'=>Str::random(10),]);
 
-        User::create(['name' => 'Mansour', 'email' => 'mansour@gmail.com', 'email_verified_at' => now(), 'password' => bcrypt('123123123'), 'remember_token' => Str::random(10),]);
 
-        User::create(['name' => 'Zain', 'email' => 'zain@gmail.com', 'email_verified_at' => now(), 'password' => bcrypt('123123123'), 'remember_token' => Str::random(10),]);
+        Conversation::create(['name'=>'Oms Group', 'uuid'=>Str::uuid(), 'user_id'=>1]);
+        Conversation::create(['name'=>'Mon01 Group', 'uuid'=>Str::uuid(), 'user_id'=>rand(1,4)]);
+        Conversation::create(['name'=>'Ran01 Group', 'uuid'=>Str::uuid(), 'user_id'=>rand(1,4)]);
+        Conversation::create(['name'=>'Helpdesk Group', 'uuid'=>Str::uuid(), 'user_id'=>rand(1,4)]);
+        Conversation::create(['name'=>'Development Group', 'uuid'=>Str::uuid(), 'user_id'=>rand(1,4)]);
 
-        User::create(['name' => 'Ali', 'email' => 'ali@gmail.com', 'email_verified_at' => now(), 'password' => bcrypt('123123123'), 'remember_token' => Str::random(10),]);
 
-        Conversation::create(['name' => 'Family Group', 'uuid' => Str::uuid(), 'user_id' => 1]);
-        Conversation::create(['name' => 'Work Group', 'uuid' => Str::uuid(), 'user_id' => rand(1, 4)]);
-        Conversation::create(['name' => 'Friends Group', 'uuid' => Str::uuid(), 'user_id' => rand(1, 4)]);
-        Conversation::create(['name' => 'Guys Group', 'uuid' => Str::uuid(), 'user_id' => rand(1, 4)]);
-        Conversation::create(['name' => 'Art Group', 'uuid' => Str::uuid(), 'user_id' => rand(1, 4)]);
+        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id'=>1, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id'=>2, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id'=>3, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id'=>4, 'created_at'=>now(), 'updated_at'=>now(),]);
 
-        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id' => 1, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id' => 2, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id' => 3, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 1, 'user_id' => 4, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 2, 'user_id' => 1, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 2, 'user_id' => 2, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 3, 'user_id' => 1, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 3, 'user_id' => 3, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 3, 'user_id' => 4, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 4, 'user_id' => 2, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 4, 'user_id' => 3, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 4, 'user_id' => 4, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 5, 'user_id' => 3, 'created_at' => now(), 'updated_at' => now(),]);
-        DB::table('conversation_user')->insert(['conversation_id' => 5, 'user_id' => 4, 'created_at' => now(), 'updated_at' => now(),]);
+
+        DB::table('conversation_user')->insert(['conversation_id' => 2, 'user_id'=>1, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 2, 'user_id'=>2, 'created_at'=>now(), 'updated_at'=>now(),]);
+
+        DB::table('conversation_user')->insert(['conversation_id' => 3, 'user_id'=>1, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 3, 'user_id'=>3, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 3, 'user_id'=>4, 'created_at'=>now(), 'updated_at'=>now(),]);
+
+        DB::table('conversation_user')->insert(['conversation_id' => 4, 'user_id'=>2, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 4, 'user_id'=>3, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 4, 'user_id'=>4, 'created_at'=>now(), 'updated_at'=>now(),]);
+
+        DB::table('conversation_user')->insert(['conversation_id' => 5, 'user_id'=>1, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 5, 'user_id'=>2, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 5, 'user_id'=>3, 'created_at'=>now(), 'updated_at'=>now(),]);
+        DB::table('conversation_user')->insert(['conversation_id' => 5, 'user_id'=>4, 'created_at'=>now(), 'updated_at'=>now(),]);
 
         $this->call(MessagesTableSeeder::class);
 
